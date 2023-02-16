46d878612464ebb4a038e97a9dd73a75097cbc43
dark07-23 <jimjani.jn@gmail.com>
2023-02-16 07:04:38 -0800

    sue

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,10 @@
  * main puts Programming is like building a multilingual puzzle
  * followed by a new line.
  * should end with value 0
- * Return: Always 0
+ * Return: Always 0.
  */
 
-int main (void)
+int main(void)
 
 {
 	puts("programming is like buiding a multilingual puzzle");
