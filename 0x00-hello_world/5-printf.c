c47f2864f42cec8b1814fe5b744398b48e47f333
dark07-23 <jimjani.jn@gmail.com>
2023-02-16 07:42:49 -0800

    and

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints "with proper grammar, but the outcome is
+ * a piece of art," followed by new line
+ * Return: Always
+ *
+ */
+
+int main(void)
+
+{
+	printf(with proper grammar,but the outcome is a piece of art,\n");
+	return (0);
+}
