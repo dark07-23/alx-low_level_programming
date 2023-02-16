d4a013a6cacf9569386f9cdeafe961ff1ce3629d
dark07-23 <jimjani.jn@gmail.com>
2023-02-16 06:47:35 -0800

    rush

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main puts Programming is like building a multilingual puzzle
+ * followed by a new line.
+ * should end with value 0
+ */
+int main (void)
+
+{
+	puts("\"programming is like buiding a multilingual puzzle");
+
+	return(0);
+}
