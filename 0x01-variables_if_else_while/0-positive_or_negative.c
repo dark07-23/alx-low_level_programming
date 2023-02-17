2a0826f738e25416dc0f93d11ff0e0893a1d7c51
dark07-23 <jimjani.jn@gmail.com>
2023-02-17 08:16:12 -0800

    33

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+#include <stdio.h>
 /**
- * dtermines if a number is possitive, zero or negative.
+ * main - entry point of program
  *
- * Return: Always 0 (success)
+ * Return: return 0 (success)
  */
 int main(void)
 {
@@ -13,17 +12,15 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%d is possitive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
+	if (n < 0)
 	{
 		printf("%d is negative\n", n);
 	}
+	else if (n > 0)
+	{
+		printf("%d is possitive\n", n);
+	}
+	else
+		printf("%d is zero\n", n);
 	return (0);
 }
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,1 @@
-this readme is about c programming
+this readme is about the tasks to follow 0. Positive anything is better than negative nothing
