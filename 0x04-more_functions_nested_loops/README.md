196da781a9d584d81a27862dc8e9dd5a29725a26
dark07-23 <jimjani.jn@gmail.com>
2023-02-23 02:33:44 -0800

    77

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+this readme is about morefunctions in nested loop
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * this is a main.h file
+ * Author: Gilbert
+ * this header has prototypes for all functions.
+ */
+int _putchar(char c);
+
+
+
+
+#endif
