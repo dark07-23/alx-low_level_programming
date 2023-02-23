acf7f5434ec516869dc1a2cc51c9e50b6b6dc14c
dark07-23 <jimjani.jn@gmail.com>
2023-02-23 02:53:38 -0800

    44

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
 /**
  * this is a main.h file
@@ -7,7 +7,17 @@
  * this header has prototypes for all functions.
  */
 int _putchar(char c);
-
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void Print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void prin_square(int size);
+void print_triangle(int size);
+void print_number(int n)
 
 
 
