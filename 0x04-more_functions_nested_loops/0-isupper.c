7cec0ae0724f33b68cc1baecfffab6dd9188a6c5
dark07-23 <jimjani.jn@gmail.com>
2023-02-24 04:37:07 -0800

    5

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,17 +8,9 @@
  */
 
 int _isupper(int c)
-
 {
-char uppercase = 'A';
-int isuppr = 0;
-for (; uppercase <= 'Z'; uppercase++)
-{
-if (c == uppercase)
-{
-isupper = 1;
-break;
-}
-}
-return (isupper);
+	if (c >= 65 && c <= 90)
+		_putchar(1);
+	else
+		_putchar(0);
 }
