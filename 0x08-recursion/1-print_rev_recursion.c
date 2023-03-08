2a8ec2ca5101d24811a0a852f4aa0ba4a6a69404
dark07-23 <jimjani.jn@gmail.com>
2023-03-08 09:26:10 -0800

    xue

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
- * main - function that prints a string in reverse.
- * @s: string to be printed
- * Auth: Gilbert
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: The string to be printed.
  */
 void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
 		_print_rev_recursion(s + 1);
-		putchar(*s);
-	}
+		_putchar(*s);
+	}	
 }
