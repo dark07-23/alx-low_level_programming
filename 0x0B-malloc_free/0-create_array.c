035b7399ce55d11b9d91d3970c40e9b875bfcd80
dark07-23 <jimjani.jn@gmail.com>
2023-03-14 02:47:07 -0700

    ff

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,11 +14,11 @@ char *create_array(unsigned int size, char c)
 if (size == 0) {
 return NULL;
 }
-char *array = malloc(size * sizeof(char));
+char *array = (char)malloc(size * sizeof(char));
 if (array == NULL) {
 return NULL;
 }
-for (int i = 0; i < size; i++) {
+for (unsigned int i = 0; i < size; i++) {
 array[i] = c;
 }
 return array;
