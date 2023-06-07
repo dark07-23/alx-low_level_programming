6c4b97984dbcb9e588ce0db6eed885c510d00f97
dark07-23 <jimnaji.jn@gmail.com>
2023-06-07 08:56:10 +0300

    third

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,17 +1,21 @@
-#include "main.h"
-/**
- *_strlen_recursion - Returns the length of a string.
- *eturn: The length of the string.
- */
-int _strlen_recursion(char *s)
-{
-int l = 0;
-
-if (*s)
-{
-l++;
-l += _strlen_recursion(s + 1);
-}
-return (longit);
+#include "main.h"                                                                                                                       
+                                                                                                                                        
+/**                                                                                                                                     
+ *  * _strlen_recursion - Returns the length of a string.                                                                                  
+ *   * @s: The string to be measured.                                                                                                       
+ *    *                                                                                                                                      
+ *     * Return: The length of the string.                                                                                                    
+ *      */                                                                                                                                     
+int _strlen_recursion(char *s)                                                                                                          
+{                                                                                                                                       
+	        int len = 0;                                                                                                                    
+		                                                                                                                                        
+		        if (*s)                                                                                                                         
+				        {                                                                                                                               
+						                len++;                                                                                                                  
+								                len += _strlen_recursion(s + 1);                                                                                        
+										        }                                                                                                                               
+			                                                                                                                                        
+			        return (len);                                                                                                                   
 }
 
