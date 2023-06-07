f0ffa9f16de0d2aa8c7b4c89f3a7794255b63058
dark07-23 <jimnaji.jn@gmail.com>
2023-06-07 08:51:02 +0300

    second trial

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,21 +1,31 @@
 #include "main.h"
 
-int find_strlen(char *s);
-int check_palindrome(char *s, int len, int index);
-int is_palindrome(char *s);
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
 
 /**
- * find_strlen - Returns the length of a string.
+ * is_palindrome - checks if a string is a palindrome
+ * Return: 1 if it is, 0 it's not
  */
-int find_strlen(char *s)
+int is_palindrome(char *s)
 {
-int len = 0;
-
-if (*(s + len))
-{
-len++;
-len += find_strlen(s + len);
+		if (*s == 0)
+					return (1);
+		return (check_pal(s, 0, _strlen_recursion(s)));
 }
-return (len);
+
+int _strlen_recursion(char *s)
+{
+		if (*s == '\0')
+					return (0);
+			return (1 + _strlen_recursion(s + 1));
+}
+int check_pal(char *s, int i, int len)
+{
+		if (*(s + i) != *(s + len - 1))
+					return (0);
+			if (i >= len)
+						return (1);
+				return (check_pal(s, i + 1, len - 1));
 }
 
--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,18 +1,17 @@
 #include "main.h"
-
 /**
- * _strlen_recursion - Returns the length of a string.
- * @s: The string
- * Return: The length g.
- *      */
+ *_strlen_recursion - Returns the length of a string.
+ *eturn: The length of the string.
+ */
 int _strlen_recursion(char *s)
 {
 int l = 0;
+
 if (*s)
 {
 l++;
 l += _strlen_recursion(s + 1);
 }
-return (len);
+return (longit);
 }
 
