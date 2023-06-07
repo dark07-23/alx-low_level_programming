e040774049b51b590b9ba105074d295a45e69524
dark07-23 <jimnaji.jn@gmail.com>
2023-06-07 08:20:10 +0300

    all in one

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,22 @@
+nclude "main.h"
+
+int is_divisible(int num, int div);
+int is_prime_number(int n);
+
+/**
+ * is_divisible - Checks if a number is divisible.
+ * @num: The number to be checked.
+ * @div: The divisor.
+ *
+ * Return: 0 If the number is divisible.
+ *         -1 If the number is not divisible.
+ */
+int is_divisible(int num, int div)
+{
+if (num % div == 0)
+return (0);
+if (div == num / 2)
+return (1);
+return (is_divisible(num, div + 1));
+}
+
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+This is about recusion and how to impliment recusive functions in a program in ALX class
