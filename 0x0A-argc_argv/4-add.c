fd37e8b4997b7376a27dc2efbc6a96e12c6e16d6
dark07-23 <jimnaji.jn@gmail.com>
2023-06-09 17:33:28 +0300

    dues

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -4,46 +4,47 @@
 #include <string.h>
 /**
  * check_num - check - string there are digit
-	 	 	 	 	 * Return: Always 0 (Success)
- *	 	 	 	 */
+ * @str: array str
+ * Return: Always 0 (Success)
+	 * 	 	 	 	 	 */
 int check_num(char *str)
 {
 	unsigned int count = 0;
+
 	while (count < strlen(str)) 
 
-	if (!isdigit(str[count])) 
 	{
-		return (0);
-		count++;
+	if (!isdigit(str[count])) 
+		{
+			return (0);
+		}
+																	count++;
 	}
-	return (1);
-}
+		return (1);
+												}
 
 
 int main(int argc, char *argv[])
-{
-		int count, int s_to_i;
+	{
+		int count;
+		int str_to_int;
 		int sum = 0;
 		count = 1;
-		
 	while (count < argc) 
 	{
-	if (check_num(argv[count]))
+		if (check_num(argv[count]))
 		{
-			s_to_i = atoi(argv[count]);
-			sum += s_to_i;
+			str_to_int = atoi(argv[count]);
+				sum += str_to_int;
 		}
-	else
-		{
-			printf("Error\n");
+
+	{
+		printf("Error\n");
 			return (1);
-		}
-	count++;
 	}
-
-printf("%d\n", sum); /*print sum*/
-
-return (0);
-}
-
+		count++;
+	}
+		printf("%d\n", sum); /*print sum*/
+			return (0);
+	}
 
