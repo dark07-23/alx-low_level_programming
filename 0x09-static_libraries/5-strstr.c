8b12a658d15555f2b18bc23d5f975ead653863cc
dark07-23 <jimnaji.jn@gmail.com>
2023-06-09 12:10:51 +0300

    trial

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+char *_strstr(char *haystack, char *needle)
+{
+		for (; *haystack != '\0'; haystack++)
+				{
+							char *l = haystack;
+									char *p = needle;
+
+													if (*p == '\0')
+																	return (haystack);
+														}
+
+
+			return (0);
+}
+
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,2 @@
+This is about static libraries in alx functions
+all about the functions in the program
