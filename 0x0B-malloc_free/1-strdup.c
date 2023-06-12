47e1328e942beb1b7a3886362ca6ecbf5cdbb998
dark07-23 <jimnaji.jn@gmail.com>
2023-06-12 10:08:11 +0300

    pure

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -6,33 +6,41 @@
  * _strdup - duplicate to new memory space location
  * 		 		 		
  * Return: 0
- */
-  
+ * 		 		 		 		 */
 		
 char *_strdup(char *str)
-			
 {
-			
 	char *a;
 	int i, j = 0;
-					
+
 	if (str == NULL)
 		return (NULL);
 						
-	i = 0;
+					i = 0;
 							
-	while (str[i] != '\0')
-		i++;
+						while (str[i] != '\0')
+									
+									i++;
 								
 
-	a = malloc(sizeof(char) * (i + 1));
-					
-	if (a == NULL)
-		return (NULL);
+								
+							a = malloc(sizeof(char) * (i + 1));
+									
+
+									
+								if (a == NULL)
+											
+											return (NULL);
 										
-	for (j = 0; str[r]; j++)
-		a[j] = str[j];
-			return (a);
+
+										
+									for (j = 0; str[j]; j++)
+												
+											a[j] = str[j];
+											
+
+											
+										return (a);
 												
 }
 
